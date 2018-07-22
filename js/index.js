87e6a93bb1e46706d1ba7ff6cc0c038013c43315
huyao <2385249676@qq.com>
2018-07-22 23:33:56 +0800

    完成点击空格的拓展

--- js/index.js
+++ js/index.js
@@ -3,30 +3,79 @@ var mine = {
     col: 0,
     row: 0,
     dommap: null,
-    init: function(domwarpper, row, col, mineProbability){
+    numberStyle: [],
+    init: function (domwarpper, row, col, mineProbability) {
         //默认30列，6行
         this.col = col || 16;
-        this.row = row || 30; 
-        mineProbability = mineProbability || 0.1;   //生成雷的概率默认是0.1        
+        this.row = row || 30;
+        mineProbability = mineProbability || 0.2;   //生成雷的概率默认是0.1        
 
         this.createmines(mineProbability);      //生成二维数组
         console.log(this.minemap)
 
         this.createDom(domwarpper);     //生成dom
 
-        this.showmine(this.dommap);     //展示地雷
-        
+        // this.showmine(this.dommap);     //展示地雷
+        this.bindEvent();
+    },
+    //绑定事件
+    bindEvent: function () {
+        var self = this;
+        // var isTheFirstClick = true;
+        this.dommap.onclick = function (e) {
+            var target = e.target;
+            // if(isTheFirstClick){
+            //     y = target.getAttribute('y'),
+            //     x = target.getAttribute('x');
+            //     if(minemap[y][x] == 10){
+                    
+            //     }
+            // }
+            
+            if (target.getAttribute('class') == 'block') {
+                self.touch(target);
+            }
+        }
+    },
+    touch: function (target) {
+        if(!target){
+            return;
+        }
+        var value = target.getAttribute('value'),
+            y = target.getAttribute('y'),
+            x = target.getAttribute('x');
+            y = +y;
+            x = +x;
+        var col = this.col,
+            row = this.row;
+        if (value == 0) {    //空地，需要拓展开来
+            target.innerHTML = '<span></span>';
+            (y > 0) && this.touch(this.getdom(y - 1, x));  //上方
+            (x < row - 1) && this.touch(this.getdom(y, x + 1)); //右方
+            (y < col - 1) && this.touch(this.getdom(y + 1, x));  //下方
+            (x > 0) && this.touch(this.getdom(y, x - 1));  //左方
+            (x > 0 && y > 0) && this.touch(this.getdom(y - 1, x - 1));   //左上方
+            (x < row - 1 && y > 0) && this.touch(this.getdom(y - 1, x + 1)); // 右上方
+            (x > 0 && y < col - 1) && this.touch(this.getdom(y + 1, x - 1));   //左下方
+            (x < row - 1 && y < col - 1) && this.touch(this.getdom(y + 1, x + 1)); //右下方
+        } else if (value == 10) {
+            this.showmine();
+        } else {
+            target.setAttribute('class', 'block number');
+            target.innerHTML = '<span class="point' + value + '">' + value + '</span>';
+        }
     },
     //生成雷区地图的二维数组
     createmines: function (mineProbability) {
-        col = this.col;
-        row = this.row;
+        var col = this.col,
+            row = this.row;
         map = this.minemap;
         //先生成空地图
         for (let y = 0; y < col; y++) {
             map[y] = new Array(row);
             map[y].fill(0);
         }
+
         //对空地图进行随机放雷和标记周围值
         for (let y = 0; y < col; y++) { //y 为纵向坐标
             for (let x = 0; x < row; x++) { //x 为横向坐标
@@ -47,33 +96,12 @@ var mine = {
             }
         }
     },
-    //展示出所有的雷
-    showmine: function (dommap) {
-        var col = this.col,
-            row = this.row;
-        var minemap = this.minemap;
-        //如果炸弹被引爆，将所有的炸弹都展示出来
-        for (let y = 0; y < col; y++) { //y 为纵向坐标
-            var domcol = dommap.children[y];
-            domcol = domcol.getElementsByClassName('row')[0];
-            for (let x = 0; x < row; x++) { //x 为横向坐标
-                if (minemap[y][x] == 10) {
-                    domcol.children[x].innerHTML = '<img src="./img/blood.bmp" alt="">';
-                }
-            }
-        }
-    },
-    //绑定事件
-    bindEvent: function () {
-        dommap.onclick = function (e) {
-            console.log(e)
-        }
-    },
+
     //创建雷区dom
     createDom: function (wrapper) {
         //创建 ul.col > li * col > ul.row > li * row的结构
-        col = this.col;
-        row = this.row;
+        var col = this.col,
+            row = this.row;
         var fragment = document.createDocumentFragment();
         //创建最大的ul.col， 包裹的所有行
         var colUl = document.createElement('ul');
@@ -89,6 +117,10 @@ var mine = {
             for (let x = 0; x < row; x++) { //x 为横向坐标
                 //创建每一个块
                 var rowLi = document.createElement('li');
+                rowLi.setAttribute('y', y);
+                rowLi.setAttribute('x', x);
+                rowLi.setAttribute('value', this.minemap[y][x]);
+                rowLi.setAttribute('class', 'block')
                 rowUl.appendChild(rowLi);
             }
             colLi.appendChild(rowUl);
@@ -97,9 +129,41 @@ var mine = {
         this.dommap = colUl;
         wrapper.innerHTML = "";
         wrapper.appendChild(fragment);
+    },
+
+    //展示出所有的雷
+    showmine: function () {
+        var dommap = this.dommap;
+        var col = this.col,
+            row = this.row;
+        var minemap = this.minemap;
+        //如果炸弹被引爆，将所有的炸弹都展示出来
+        for (let y = 0; y < col; y++) { //y 为纵向坐标
+            var domcol = dommap.children[y];
+            domcol = domcol.getElementsByClassName('row')[0];
+            for (let x = 0; x < row; x++) { //x 为横向坐标
+                if (minemap[y][x] == 10) {
+                    domcol.children[x].innerHTML = '<img src="./img/blood.bmp" alt="">';
+                }
+            }
+        }
+    },
+    //获取坐标对应dom
+    getdom(y, x) {
+        var dommap = this.dommap,
+            minemap = this.minemap;
+        var col = this.col,
+            row = this.row;
+        var domcol = dommap.children[y];
+        domcol = domcol.getElementsByClassName('row')[0];
+        if(minemap[y][x] > 10 ){
+            return null;
+        } else {
+            minemap[y][x]  = 100;
+            return domcol.children[x];
+        }
     }
 }
-
 
 var domwarpper = document.getElementsByClassName('mines')[0];
 
